1580f9237a4ca9a36d09009a944aa647417c6344
as178 <shadowangel753@gmail.com>
2025-08-21 00:10:24 +1200

    drawer navigation implemented alongside stack navigation, utilising and expanding upon previous code in more depth

--- App.jsx
+++ App.jsx
@@ -1,36 +1,73 @@
+import "react-native-gesture-handler";
 import HomeScreen from "./screens/HomeScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import UserScreen from "./screens/UserScreen";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  useNavigation,
+  DrawerActions,
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Icon from "react-native-vector-icons/Entypo";
+import DrawerContent from "./DrawerContent";
 
-function App() {
+const StackNav = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          statusBarBackgroundColor: "orange",
-          title: "Home Screen",
-          headerStyle: {
-            backgroundColor: "orange",
-          },
-          headerTintColor: "white",
-          headerTitleAlign: "center",
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        statusBarBackgroundColor: "blue",
+        title: "Home Screen",
+        headerStyle: {
+          backgroundColor: "blue",
+        },
+        headerTintColor: "white",
+        headerTitleAlign: "center",
+        headerLeft: () => {
+          return (
+            <Icon
+              name="menu"
+              onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+              size={30}
+              color="rgba(255, 255, 255, 1)"
+            />
+          );
+        },
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          headerShown: true,
         }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} options={{}} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen
-          name="User"
-          component={UserScreen}
-          options={{
-            headerShown: true,
-          }}
-        />
-      </Stack.Navigator>
+      />
+    </Stack.Navigator>
+  );
+};
+
+const DrawerNav = () => {
+  const Drawer = createDrawerNavigator();
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <DrawerContent {...props} />}
+      screenOptions={{ headerShown: false }}
+    >
+      <Drawer.Screen name="Home" component={StackNav} />
+    </Drawer.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNav />
     </NavigationContainer>
   );
 }
